5e1e1fa50233eac0bd7706506b7e55558957ce1f
EC2 Default User <ec2-user@ip-172-31-10-226.us-east-2.compute.internal>
2018-03-12 10:39:11 +0000

    databases

--- rail_projects/test_app/config/routes.rb
+++ rail_projects/test_app/config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
+  root "pages#home"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
   get '/pages/about/', to: 'pages#about'
   resources :users
   resources :articles
-  rosources :pages
+  resources :pages
 end
---
+++ rail_projects/test_app/db/migrate/20180312101329_crate_articles.rb
@@ -0,0 +1,9 @@
+class CrateArticles < ActiveRecord::Migration[5.1]
+  def change
+    create_table :articles do|t|
+     t.string :title
+     t.text :description
+     t.timestamps
+   end
+  end
+end
